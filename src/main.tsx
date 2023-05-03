15ed5f9fb69bbcda8cd2c5db17e8815b5adbd2b8
johnsamuel20 <105310819+johnsamuel20@users.noreply.github.com>
2023-05-03 13:15:23 +0300

    Registeration Form (#4) (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Form from './Pages/Form/Form.tsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/form',
+    element: <Form />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
