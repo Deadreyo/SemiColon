3ece06ad05c7020baaf3f9fff8094d9ac0e435cf
johnsamuel20 <joe.samuel.john.js@gmail.com>
2023-05-08 20:32:29 +0300

    modified:   src/Components/SmoothScroll/SmoothScroll.tsx

--- src/Components/SmoothScroll/SmoothScroll.tsx
+++ src/Components/SmoothScroll/SmoothScroll.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useLocation, useNavigationType } from 'react-router-dom'
-function SmoothScroll({ children }) {
+function SmoothScroll({ children }: { children: React.ReactNode }) {
   const location = useLocation()
   const navType = useNavigationType()
   useEffect(() => {
