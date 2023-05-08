9f1936ce7573da8361ecad93f28ce22e7abd05be
johnsamuel20 <joe.samuel.john.js@gmail.com>
2023-05-08 20:28:18 +0300

    modified:   src/Components/SubmitError/SubmitError.tsx

--- src/Components/SubmitError/SubmitError.tsx
+++ src/Components/SubmitError/SubmitError.tsx
@@ -4,7 +4,7 @@ function SubmitError() {
   return (
     <div>
       <div className="error-title">
-        <h2>Something went wrong , Try Again </h2>
+        <h2>Something went wrong, Try Again </h2>
       </div>
       <button className="submit">
         <a className="btn-title" href="/">
