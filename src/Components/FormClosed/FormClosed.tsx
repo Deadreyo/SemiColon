c543fe23bcbc37cfbe3e66be2ae7bda24c0c836f
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-05-18 21:59:43 +0300

    feat: update form closing message

--- src/Components/FormClosed/FormClosed.tsx
+++ src/Components/FormClosed/FormClosed.tsx
@@ -3,7 +3,9 @@ function FormClosed() {
   return (
     <div>
       <div className="form-unavailable-message">
-        <h2>Sorry, this form isn't available right now</h2>
+        <h2>Sorry, workshop registration has ended.</h2>
+        <br />
+          Don't be sad though! Registration will open again after the finals! Follow our <a href="https://www.facebook.com/elite.team.asu" target="_blank">Facebook page</a> to stay updated.
       </div>
       <div className="ball-loader">
         <div className="bar">
