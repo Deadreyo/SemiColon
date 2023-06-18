53e20b6220bf2a334d2b801dfc7b0a3db7858698
NightKnight <Ahmedatwa866@yahoo.com>
2023-06-18 14:12:29 +0300

    formatting

--- src/TrackData/FrontEnd.ts
+++ src/TrackData/FrontEnd.ts
@@ -1,26 +1,25 @@
 import FrontEndImage from '../assets/Web.jpg'
 const frontEnd = {
-  "title": "Frontend Development",
-  "description": "HTML, CSS, and JavaScript are the three core technologies that power the modern web. This workshop introduces you to their fundamentals, so you can start building your own websites and web apps. Learn to structure web pages with HTML, style them with CSS, and add interactivity with JavaScript.",
-  "objectives": [
-    "1. You will start your way in Web Development world by a very powerful and easy techniques.",
-    "2. In this course you will gain the fundamental and basic concepts of Front-end web development, you will have fundamental knowledge of HTML/CSS syntax.",
-    "3. You will learn the basics of JavaScript and how to use it to add interactivity and dynamic behavior to web pages."
+  title: 'Frontend Development',
+  description:
+    'HTML, CSS, and JavaScript are the three core technologies that power the modern web. This workshop introduces you to their fundamentals, so you can start building your own websites and web apps. Learn to structure web pages with HTML, style them with CSS, and add interactivity with JavaScript.',
+  objectives: [
+    '1. You will start your way in Web Development world by a very powerful and easy techniques.',
+    '2. In this course you will gain the fundamental and basic concepts of Front-end web development, you will have fundamental knowledge of HTML/CSS syntax.',
+    '3. You will learn the basics of JavaScript and how to use it to add interactivity and dynamic behavior to web pages.',
   ],
-  "requirements": [
-    "You don’t need any skill or any previous experience!"
+  requirements: ['You don’t need any skill or any previous experience!'],
+  content: [
+    '1. Basic setup and learning how to learn',
+    '2. Web standards and best practices (such as accessibility and cross-browser compatibility)',
+    '3. HTML, the language that gives web content structure and meaning',
+    '4. CSS, the language used to style web pages',
+    '5. JavaScript, the language that adds interactivity to a web page',
+    '6. Interacting with the DOM (Document Object Model) API',
   ],
-  "content": [
-    "1. Basic setup and learning how to learn",
-    "2. Web standards and best practices (such as accessibility and cross-browser compatibility)",
-    "3. HTML, the language that gives web content structure and meaning",
-    "4. CSS, the language used to style web pages",
-    "5. JavaScript, the language that adds interactivity to a web page",
-    "6. Interacting with the DOM (Document Object Model) API"
-  ],
-  "noOfSessions": "6-8 sessions in 4-5 weeks",
-  "id": "frontEnd",
-  "image": FrontEndImage
+  noOfSessions: '6-8 sessions in 4-5 weeks',
+  id: 'frontEnd',
+  image: FrontEndImage,
 }
 
-export default frontEnd;
+export default frontEnd
